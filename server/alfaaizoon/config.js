deb2407e742807d3dab3b274e86b845b0269806e
root <root@staff.alfaaizoon.com>
2024-10-17 20:28:26 +0000

    intial from server

---
+++ server/alfaaizoon/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    REACT_APP_HOST: "https://staff.alfaaizoon.com",
+  //  REACT_APP_HOST: "http://localhost",
+    REACT_APP_PORT: ""
+    // API_URI: `http://${REACT_APP_HOST}:${REACT_APP_PORT}`
+}
